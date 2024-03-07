a28121f72541f06573439eb7433d5a6931741fad
Thomas Verderesi <tverderesi@gmail.com>
2024-03-07 18:42:14 -0300

    enhancement: added auto-skipping if the database is not empty.

--- apps/server/src/seed/index.ts
+++ apps/server/src/seed/index.ts
@@ -11,9 +11,16 @@ export async function seedVideos(uri?: string) {
 
   try {
     await mongoose.connect(mongoUri);
-    // Insert the videos into the collection
-    const result = await VideoModel.insertMany(videos);
-    console.log(`${result.length} videos seeded successfully.`);
+
+    //check if the videos collection is empty
+    const existingVideos = await VideoModel.find({});
+    if (existingVideos.length > 0) {
+      console.log("Videos collection is not empty. Skipping seeding.");
+      return;
+    } else {
+      const result = await VideoModel.insertMany(videos);
+      console.log(`${result.length} videos seeded successfully.`);
+    }
   } catch (error) {
     console.error("Error seeding videos:", error);
   } finally {
