f792762c0de8cf859707126ff856df9d44ca3c2c
Thomas Verderesi <tverderesi@gmail.com>
2024-03-07 23:59:53 -0300

    fix: removed useless call to dotenv

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+
 export default async function connectDb() {
-  dotenv.config();
   const db = await mongoose.connect(process.env.MONGO_URI as string, {});
   console.log("Connected to MongoDB.");
   db.connection.on("close", () => {
