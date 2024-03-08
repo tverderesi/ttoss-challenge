0121dec2f7cd0af663a1438a8f0ec7b374cc0b79
Thomas Verderesi <tverderesi@gmail.com>
2024-03-08 00:01:59 -0300

    fix: properly handling of seedAll depending on its context.

--- apps/server/src/seed/index.ts
+++ apps/server/src/seed/index.ts
@@ -1,5 +1,20 @@
 import { seedVideos } from "./seedVideos";
 import { seedAdmin } from "./seedAdmin";
+import mongoose from "mongoose";
+import { setEnv } from "@/config/env";
+export const seedAll = async () => {
+  await seedAdmin().catch(console.error);
+  await seedVideos().catch(console.error);
+};
 
-seedAdmin().catch(console.error);
-seedVideos().catch(console.error);
+if (require.main === module) {
+  (async () => {
+    setEnv();
+    const readyState = mongoose.connection.readyState;
+    if (readyState === 0) {
+      await mongoose.connect(process.env.MONGO_URI as string, {});
+    }
+    await seedAll();
+    process.exit(0);
+  })();
+}
