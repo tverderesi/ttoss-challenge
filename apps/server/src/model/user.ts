a8ebda5464c6ff0bc26b392f928144339bddad69
Thomas Verderesi <tverderesi@gmail.com>
2024-03-07 21:55:38 -0300

    closes #14

---
+++ apps/server/src/model/user.ts
@@ -0,0 +1,31 @@
+import { Document, Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+export interface User {
+  username: string;
+  password: string;
+  role: "user" | "admin";
+}
+
+export interface UserModel extends User, Document {}
+
+const UserSchema = new Schema<User>(
+  {
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true, default: "user" },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+UserSchema.pre<UserModel>("save", async function (next) {
+  if (this.isModified("password") || this.isNew) {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(this.password, saltRounds);
+    this.password = hashedPassword;
+  }
+  next();
+});
+
+export const UserModel = model<UserModel>("User", UserSchema);
