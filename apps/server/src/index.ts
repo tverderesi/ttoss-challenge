16aaa405fcfc94e6915d0280af3a9f116d5cd78a
Thomas Verderesi <tverderesi@gmail.com>
2024-03-08 00:10:50 -0300

    closes #16

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,19 +1,39 @@
 import express from "express";
-import dotenv from "dotenv";
+import { setEnv } from "@/config/env";
+import connectDb from "./db";
+import cookieParser from "cookie-parser";
+import { applyCors } from "@/config/cors";
+import bodyParser from "body-parser";
+import { clearDb } from "@/seed/clearDb";
+import { seedAll } from "./seed";
 
-dotenv.config();
-const app = express();
+setEnv();
 const EXPRESS_PORT = process.env.EXPRESS_PORT || 6000;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+applyCors(app, process.env.NODE_ENV);
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.listen(EXPRESS_PORT, () => {
-  console.log(`Server is running on port ${EXPRESS_PORT}`);
+  console.log("Server is running on port", EXPRESS_PORT);
 });
+
+(async () => {
+  await connectDb();
+  if (process.env.NODE_ENV !== "production") {
+    console.log(`Server is running on ${process.env.NODE_ENV} mode. Clearing database...`);
+    await clearDb(process.env.MONGO_URI);
+  }
+  await seedAll();
+})();
 
 process.on("SIGINT", () => {
   console.log("Gracefully shutting down...");
-
   process.exit(0);
 });
