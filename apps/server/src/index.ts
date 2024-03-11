766999655e2ac33216a559a89d9d5db7c7f2a0a8
Thomas Verderesi <tverderesi@gmail.com>
2024-03-10 22:06:52 -0300

    hotfix: forgot to cmmit the new env, the new scripts and the new index.ts

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -6,9 +6,10 @@ import { applyCors } from "@/config/cors";
 import bodyParser from "body-parser";
 import { clearDb } from "@/seed/clearDb";
 import { seedAll } from "./seed";
+import { bindGraphQLServer } from "./graphql";
 
 setEnv();
-const EXPRESS_PORT = process.env.EXPRESS_PORT || 6000;
+const EXPRESS_PORT = process.env.EXPRESS_PORT;
 
 const app = express();
 app.use(bodyParser.json());
@@ -26,11 +27,14 @@ app.listen(EXPRESS_PORT, () => {
 
 (async () => {
   await connectDb();
-  if (process.env.NODE_ENV !== "production") {
-    console.log(`Server is running on ${process.env.NODE_ENV} mode. Clearing database...`);
+  if (process.env.NODE_ENV !== "production" && process.env.RESEED_DB === "true") {
+    console.log(`Server is running on ${process.env.NODE_ENV} mode with RESEED_DB set to ${process.env.RESEED_DB}.`);
+    console.log("If you wish to not reseed on every run, set this parameter to false.");
+    console.log("Clearing database...");
     await clearDb(process.env.MONGO_URI);
   }
   await seedAll();
+  await bindGraphQLServer(app);
 })();
 
 process.on("SIGINT", () => {
